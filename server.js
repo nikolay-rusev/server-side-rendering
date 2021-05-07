59e218c36639f63757b72f6a2bb39f3d2fbe9c8d
nikolay-rusev <n.rusev@tinqin.com>
2021-05-07 14:59:33 +0300

    update

--- server.js
+++ server.js
@@ -1,27 +1,40 @@
 const express = require("express");
+var proxy = require("express-http-proxy");
+const fetch = require("node-fetch");
 const app = express();
 const puppeteer = require("puppeteer");
+const mainURL = "http://localhost:3090";
+
+// app.use("/", proxy(mainURL));
 
 app.get("*", async (req, res) => {
-    console.log("request url: " + req.url);
+    const pattern = /\.[0-9a-z]{1,5}$/gi;
+    const ignore = req.originalUrl.match(pattern);
 
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: ["--font-render-hinting=none", "--force-color-profile=srgb"]
-    });
-    const page = await browser.newPage();
-    await page.setUserAgent(
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
-    );
+    console.log("request url: " + req.originalUrl);
 
-    const local_url = "http://localhost:3090" + req.originalUrl;
-    await page.goto(local_url, { waitUntil: "networkidle0" });
+    // process page
+    if (!ignore) {
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ["--font-render-hinting=none", "--force-color-profile=srgb"]
+        });
+        const page = await browser.newPage();
+        await page.setUserAgent(
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
+        );
 
-    const html = await page.evaluate(() => {
-        return document.documentElement.innerHTML;
-    });
+        const local_url = mainURL + req.originalUrl;
+        await page.goto(local_url, { waitUntil: "networkidle0" });
 
-    res.send(html);
+        const html = await page.evaluate(() => {
+            return document.documentElement.innerHTML;
+        });
+
+        res.send(html);
+    } else {
+        await fetch(mainURL + req.originalUrl).then(result => res.send(result.text()));
+    }
 });
 
 app.listen(3000, () => console.log(`Server is listening on port: 3000`));
