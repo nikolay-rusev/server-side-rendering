389d926b65c89c24d356da33a0fb8478c2ee347d
nikolay-rusev <n.rusev@tinqin.com>
2021-05-07 12:58:18 +0300

    update

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const app = express();
 const puppeteer = require("puppeteer");
 
 app.get("*", async (req, res) => {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ["--font-render-hinting=none", "--force-color-profile=srgb"]
+    });
     const page = await browser.newPage();
-
-    console.log("req.originalUrl: " + req.originalUrl);
+    await page.setUserAgent(
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
+    );
 
     const local_url = "http://localhost:3090" + req.originalUrl;
-    await page.goto(local_url, {
-        waitUntil: "networkidle0"
-    });
+    await page.goto(local_url, { waitUntil: "networkidle2" });
 
     const html = await page.evaluate(() => {
         return document.documentElement.innerHTML;
